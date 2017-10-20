539773bbdd303bc984c86d57359b7cd0c540af4f
up209d <up209d@gmail.com>
2017-10-20 19:28:47 +1100

    XHR request dowloading

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ Extension can be found here:
 https://chrome.google.com/webstore/detail/save-all-resources/abpdnfjocnmdomablahdcfnoggeeiedb?hl=en-US
 
 
-- 0.0.3: Bug Fixes
+- 0.0.4: Including download for assests by XHR requests.
+
+- 0.0.3: Bug Fixes.
 
 - 0.0.2: Extension now re-fetchs resource contents from browser-cache in order to improve speed & stability.
 
--- unpacked/content.js
+++ unpacked/content.js
@@ -1,8 +1,45 @@
 console.log('Hello from -> Content');
 
 document.addEventListener('DOMContentLoaded', function () {
+	
+//	chrome.devtools.network.getHAR(function(logInfo){
+//			console.log(logInfo);
+//		});
 
 	document.getElementById('up-save').addEventListener('click', saveAllResources);
+	
+	document.getElementById('check-xhr').addEventListener('change',function(e){
+		e.target.checked = !e.target.checked;
+		if (!e.target.checked) {
+			e.target.checked = false;
+			document.getElementById('label-xhr').innerHTML = 'Reloading page for collecting XHR requests ...'; //Include all assets by XHR requests
+			document.getElementById('up-save').innerHTML = 'Waiting for reload';
+			document.getElementById('up-save').disabled = true;
+			chrome.tabs.reload(chrome.devtools.inspectedWindow.tabId,null,function(){
+				setTimeout(function(){
+					chrome.tabs.executeScript(chrome.devtools.inspectedWindow.tabId, {
+						code: 'window.onload=function(){chrome.runtime.sendMessage({type: "RELOADED"})}'
+					});
+				},1000);
+				e.target.disabled = true;
+			});
+		} else {
+			e.target.checked = false;
+		}
+	});
+	
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			//	console.log(request.type,sender.tab.id,chrome.devtools.inspectedWindow.tabId)
+			if (request.type === 'RELOADED' && sender.tab.id === chrome.devtools.inspectedWindow.tabId) {
+				document.getElementById('check-xhr').checked = true;
+				document.getElementById('check-xhr').disabled = false;
+				document.getElementById('label-xhr').innerHTML = 'Include all assets by XHR requests'
+				document.getElementById('up-save').innerHTML = 'Save All Resources';
+				document.getElementById('up-save').disabled = false;
+			}
+		}
+	);
 
 	chrome.devtools.inspectedWindow.getResources(function (resources) {
 		document.getElementById('status').innerHTML = 'Resources count: ' + resources.length;
@@ -30,13 +67,26 @@ document.addEventListener('DOMContentLoaded', function () {
 		// alert("New Resource  Object is " + resource);
 		console.log('Resource commited: ', resource.url);
 	});
+	
+	
 });
+
+
 
 function saveAllResources(e) {
 
 	var toDownload = [];
 	var downloadThread = 5;
-
+	
+	var xhrResources = [];
+	if (document.getElementById('check-xhr').checked) {
+		chrome.devtools.network.getHAR(function(logInfo){
+			logInfo.entries.map(function(entry){
+				xhrResources.push(entry.request);
+			})
+		});
+	}
+	
 	// Disable download notification
 	chrome.downloads.setShelfEnabled(false);
 
@@ -44,7 +94,7 @@ function saveAllResources(e) {
 		console.log('Save content from: ', tab.url);
 		var domain = tab.url.split('://')[1].substring(0, tab.url.split('://')[1].indexOf('/'));
 		//Fetching all available resources and filtering using name of script snippet added 
-		chrome.devtools.inspectedWindow.getResources(function (resources) {
+		chrome.devtools.inspectedWindow.getResources(function(resources) {
 			//		resources.map(function (item) {
 			//			console.log(item);
 			//		})
@@ -54,47 +104,60 @@ function saveAllResources(e) {
 			// Disable button
 			e.target.innerHTML = 'Starting Download';
 			e.target.disabled = true;
-
+			
+			var combineResources = xhrResources.concat(resources);
+			
 			if (document.getElementById('check-all').checked) {
-				for (i = 0; i < resources.length; i++) {
-					if (resources[i].url.search(/^(http|https)/) !== -1) {
-						toDownload.push(resources[i]);
+				for (i = 0; i < combineResources.length; i++) {
+					if (combineResources[i].url.search(/^(http|https)/) !== -1) {
+						// Make sure unique URL
+						if (toDownload.findIndex(function(item){return item.url === combineResources[i].url}) === -1) {
+							toDownload.push(combineResources[i]);
+						}
 					}
 				}
 			} else {
-				for (i = 0; i < resources.length; i++) {
+				for (i = 0; i < combineResources.length; i++) {
 					// Matching with current snippet URL
-					if (resources[i].url.indexOf('://' + domain) >= 0) {
-						toDownload.push(resources[i]);
+					if (combineResources[i].url.indexOf('://' + domain) >= 0) {
+						// Make sure unique URL
+						if (toDownload.findIndex(function(item){return item.url === combineResources[i].url}) === -1) {
+							toDownload.push(combineResources[i]);
+						}
 					}
 				}
 			}
 			
+			console.log(toDownload)
+			
 			downloadListWithThread(toDownload, downloadThread, function () {
-				// Re-enable Download notification
-				chrome.downloads.setShelfEnabled(true);
-
-				var endStatus = document.createElement('p');
-				endStatus.className = 'all-done';
-				endStatus.innerHTML = 'Downloaded All Files !!!';
-				document.getElementById('debug').insertBefore(endStatus, document.getElementById('debug').childNodes[0]);
-
-				var openDownload = document.createElement('button');
-				openDownload.innerHTML = 'Open';
-				openDownload.addEventListener('click', function () {
-					chrome.downloads.showDefaultFolder();
-				});
-
-				document.getElementById('status').innerHTML = 'Resources Folder: ';
-				document.getElementById('status').appendChild(openDownload);
-
-				e.target.innerHTML = 'Re-Download?';
-				e.target.disabled = false;
-
+				allDone();
 			});
 
 		});
 	})
+}
+
+function allDone() {
+	// Re-enable Download notification
+	chrome.downloads.setShelfEnabled(true);
+
+	var endStatus = document.createElement('p');
+	endStatus.className = 'all-done';
+	endStatus.innerHTML = 'Downloaded All Files !!!';
+	document.getElementById('debug').insertBefore(endStatus, document.getElementById('debug').childNodes[0]);
+
+	var openDownload = document.createElement('button');
+	openDownload.innerHTML = 'Open';
+	openDownload.addEventListener('click', function () {
+		chrome.downloads.showDefaultFolder();
+	});
+
+	document.getElementById('status').innerHTML = 'Resources Folder: ';
+	document.getElementById('status').appendChild(openDownload);
+
+	document.getElementById('up-save').innerHTML = 'Re-Download?';
+	document.getElementById('up-save').disabled = false;
 }
 
 function downloadListWithThread(toDownload, threadCount, callback) {
@@ -134,7 +197,7 @@ function downloadURLs(urls, callback) {
 			resolved: false
 		});
 		
-		if (document.getElementById('check-cache').checked) {
+		if (document.getElementById('check-cache').checked && currentURL.getContent) {
 			currentURL.getContent(function (content, encoding) {
 				var currentEnconding = encoding;
 				if (filename.search(/\.(png|jpg|jpeg|gif|ico|svg)/) !== -1) {
