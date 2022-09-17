9a0d5ee40b83ab3c5e2dd53ccd4f82b4665cf552
Duc Duong <duc.duong@go1.com>
2022-09-17 23:41:38 +1000

    update manifest v3 for unpacked1x

--- src/devtoolApp/components/Header/index.js
+++ src/devtoolApp/components/Header/index.js
@@ -1,16 +1,12 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React from 'react';
 import { withTheme } from 'styled-components';
 import { HeaderWrapper } from './styles';
 import ResetButton from 'devtoolApp/components/ResetButton';
 import Button from 'devtoolApp/components/Button';
 import { useStore } from 'devtoolApp/store';
-import { downloadZipFile, resolveDuplicatedResources } from 'devtoolApp/utils/file';
-import { logResourceByUrl } from '../../utils/resource';
-import * as uiActions from 'devtoolApp/store/ui';
 import { INITIAL_STATE as UI_INITIAL_STATE } from 'devtoolApp/store/ui';
-import { resetNetworkResource } from 'devtoolApp/store/networkResource';
-import { resetStaticResource } from 'devtoolApp/store/staticResource';
 import { useAppSaveAllResource } from '../../hooks/useAppSaveAllResource';
+import packageJson from '/package.json';
 
 export const Header = (props) => {
   const { state } = useStore();
@@ -22,7 +18,7 @@ export const Header = (props) => {
     <HeaderWrapper>
       <div>
         <span>Resources Saver</span>
-        <sup>Version: 2.0.0</sup>
+        <sup>Version: {packageJson?.version || 'LOCAL'}</sup>
         <ResetButton color={props.theme.white} bgColor={props.theme.danger} />
       </div>
       <Button onClick={handleOnSave} disabled={status !== UI_INITIAL_STATE.status || isSaving}>
